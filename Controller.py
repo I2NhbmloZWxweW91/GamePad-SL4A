c61c99d023c66ef7680641c9f3396d75a8233841
HD Dananjaya <I2NhbmloZWxweW91@gmail.com>
2018-03-13 08:32:54 +0530

    Add files via upload

---
+++ Controller.py
@@ -0,0 +1,96 @@
+"""
+ -- Run this on PC -- 
+Usage : press A D W S keys according to X Y values
+By    : @_hddananjaya
+"""
+
+import socket,time            
+from directkeys import *
+import threading
+
+# assign hex vals
+S = 0x1F
+W = 0x11
+A = 0x1E
+D = 0x20
+
+# wait till user ready
+time.sleep(5) 
+
+HOST = "192.168.99.3"
+PORT = 5555 
+conn = socket.socket()                       
+conn.connect((HOST, PORT))
+
+aY = 0
+aX = 0
+
+def controlTurning(aY):
+   if (aY > -1.2 and aY < 1.2):
+      #print ("no turn "+str(aY))
+      try:
+         ReleaseKey(A)
+         ReleaseKey(D)
+      except:
+         pass
+   
+   elif (aY < -1.2):    
+      PressKey(A)
+      time.sleep(abs(aY)*0.01)
+      ReleaseKey(A)
+
+   elif (aY > 1.2):  
+      PressKey(D)
+      time.sleep(abs(aY)*0.01)
+      ReleaseKey(D)
+         
+def controlGasAndBreak(aX):
+   if (aX > -0.7 and aX < 1.7):
+      #print ("no gas "+str(aX))
+      try:
+         ReleaseKey(W)
+         ReleaseKey(S)
+      except:
+         pass
+      
+   elif (aX < -0.7):    
+      PressKey(W)
+      time.sleep(abs(aY)*0.01)
+      ReleaseKey(S)
+      
+   elif (aX > 1.9):  
+      PressKey(S)
+      time.sleep(abs(aY)*0.01)
+      ReleaseKey(W)
+
+def main():
+   while True:
+      
+      try :
+         txt = conn.recv(1024).decode('utf8')
+         #print (txt)
+         recvData = txt.split(",")
+
+         """
+         aX : acceleration of X axis 
+         aY : acceleration of Y axis
+         """
+         
+         _aX = float(recvData[0])
+         _aY = float(recvData[1])
+
+         controlTurning(_aY)
+         controlGasAndBreak(_aX)
+
+         time.sleep(0.01)
+         
+      except KeyboardInterrupt:
+         conn.close ()
+      except Exception as e:
+         print (e)
+
+   
+
+if __name__ == '__main__':
+   main()
+   
---
+++ Sender.py
@@ -0,0 +1,38 @@
+"""
+ -- Run this on Android -- 
+Usage : send X Y values of the accelometer
+By    : @_hddananjaya
+"""
+
+import socket as skt 
+import android, time
+
+s= skt.socket()
+host = skt.gethostname()
+port = 5555
+
+s.bind(("",port))
+s.listen(5)
+c, addr = s.accept()
+
+
+droid = android.Android()
+droid.startSensingTimed(1, 250)
+time.sleep(1)
+ 
+while 1:
+   try:
+      droid.readSensors().result
+      data = droid.sensorsReadAccelerometer().result
+      aX = data[0]
+      aY = data[1] 
+      
+      sendData = str(aX)+","+str(aY)
+      print (aX,aY)
+      c.send(sendData.encode('utf8')) 
+      time.sleep(0.1)
+   
+   except:
+      c.close()
+      droid.stopSensing()
+      exit
